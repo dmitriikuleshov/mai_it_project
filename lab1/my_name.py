c2ef99328d2506ef57e43f6bb3f2b0317b2c370d
Михаил <mmvlasko@mai.education>
2024-03-13 20:14:19 +0300

    Merged last 3 commits.

--- lab1/my_name.py
+++ lab1/my_name.py
@@ -1,6 +1,20 @@
-def print_my_name(first_name):
-    print(f'My name is {first_name}.')
+def print_my_name(first_name, last_name):
+    """
+    Prints your name.
+    :param last_name: str
+    :param first_name: str
+    :return: None
+    """
+    print(f'My name is {first_name} {last_name}.')
 
 
 def main():
-    print_my_name('Mihail')
+    """
+    Main function
+    :return: None
+    """
+    print_my_name('Mihail', 'Vlasko')
+
+
+if __name__ == '__main__':
+    main()
