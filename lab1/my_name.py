c03026f46719c4dfa43efe5d79e5062717fec006
Михаил <mmvlasko@mai.education>
2024-03-13 21:15:36 +0300

    Revert "Merged last 3 commits."
    This reverts commit c2ef99328d2506ef57e43f6bb3f2b0317b2c370d.

--- lab1/my_name.py
+++ lab1/my_name.py
@@ -1,20 +1,6 @@
-def print_my_name(first_name, last_name):
-    """
-    Prints your name.
-    :param last_name: str
-    :param first_name: str
-    :return: None
-    """
-    print(f'My name is {first_name} {last_name}.')
+def print_my_name(first_name):
+    print(f'My name is {first_name}.')
 
 
 def main():
-    """
-    Main function
-    :return: None
-    """
-    print_my_name('Mihail', 'Vlasko')
-
-
-if __name__ == '__main__':
-    main()
+    print_my_name('Mihail')
